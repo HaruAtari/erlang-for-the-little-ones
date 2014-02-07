65871a3d2e189d2896caf8f84f9aedce07b56f32
Haru Atari <HaruAtari@gmail.com>
2014-02-07 19:17:57 +0400

    Глава 3: исправлены опечатки.

--- 03/sources/chapter03.erl
+++ 03/sources/chapter03.erl
@@ -68,7 +68,9 @@ assessment_of_temp(Temp) ->
         {X, kelvin} when 293 =< X, X =< 318 ->
             'scientifically favorable';
         {X, fahrenheit} when 68 =< X, X =< 113 ->
-            'favorable in the US'
+            'favorable in the US';
+        _ ->
+            'not the best tempperature'
   end.
 
 
